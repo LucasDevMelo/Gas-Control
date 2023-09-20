16b4f84c981330ef37d8cf07ba002c0879984ef4
lucas.lopess <lucaslopesdemelosilva@gmail.com>
2023-09-20 10:32:12 -0300

    Atualizacao 20/09/2023

--- app/src/main/java/com/LoopsSoftwares/consumocombustivel/MainActivity.kt
+++ app/src/main/java/com/LoopsSoftwares/consumocombustivel/MainActivity.kt
@@ -2,10 +2,33 @@ package com.LoopsSoftwares.consumocombustivel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.LoopsSoftwares.consumocombustivel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateBtn.setOnClickListener {
+            calculateConsumption()
+        }
+    }
+
+    private fun calculateConsumption(){
+        val kilometers = binding.kilometersEdit.text.toString().toFloatOrNull()
+        val liters = binding.litersEdit.text.toString().toFloatOrNull()
+
+        if (kilometers != null && liters != null){
+            val consumption = kilometers/liters
+            val consumptionResult = String.format("%.2f", consumption)
+
+            binding.resultText.text = "Consumo médio: $consumptionResult"
+        } else {
+            binding.resultText.text = "Dados inválidos!"
+        }
     }
 }
